e345595ae0b4b9a238110d212a06f40d53646128
ishwar <contactkepper.com>
2021-02-01 17:14:21 +0530

    Integrated simple html code with react(homepage)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="index.html">amazona</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3>Sopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/download.jpg"
+                  alt="Shirt"
+                />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Miacawer</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Review)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">All right reserved.</footer>
     </div>
   );
 }
