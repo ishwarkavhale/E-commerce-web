d3e517144b90b10e6de98d642ad6b75f202aa74a
ishwar <contactkepper.com>
2021-02-02 08:35:24 +0530

    basic changes

---
+++ Frontend/src/Data.js
@@ -0,0 +1,51 @@
+const Data = {
+  products: [
+    {
+      name: 'Slim Shirt',
+      category: 'shirts',
+      image: './images/download.jpg',
+      brand: 'Nike',
+      price: 60,
+      rating: 4.5,
+      numReview: 10,
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'shirts',
+      image: './images/download.jpg',
+      brand: 'Levis',
+      price: 67,
+      rating: 4.2,
+      numReview: 15,
+    },
+    {
+      name: 'Best Pants',
+      category: 'pants',
+      image: './images/download.jpg',
+      brand: 'Nike',
+      price: 50,
+      rating: 3.9,
+      numReview: 11,
+    },
+    {
+      name: 'T-Shirt',
+      category: 'shirts',
+      image: './images/download.jpg',
+      brand: 'Nike',
+      price: 60,
+      rating: 9.6,
+      numReview: 10,
+    },
+    {
+      name: 'lower Pant',
+      category: 'pants',
+      image: './images/download.jpg',
+      brand: 'Miacawer',
+      price: 61,
+      rating: 4.0,
+      numReview: 8,
+    },
+  ],
+};
+
+export default Data;
