cd15ae0a7edb7032ce3c0520c53bb9edefd44c12
ishwar <contactkepper.com>
2021-02-02 13:00:15 +0530

    Product Details Page Created

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Data from './Data';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   const openMenu = () => {
     document.querySelector('.sidebar').classList.add('open');
@@ -8,59 +10,41 @@ function App() {
     document.querySelector('.sidebar').classList.remove('open');
   };
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">amazona</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Sopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            {Data.products.map((product) => (
-              <li>
-                <div className="product">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                    alt="Shirt"
-                  />
-                  <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                  </div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">${product.price}</div>
-                  <div className="product-rating">
-                    {' '}
-                    {product.rating} Stars ({product.numReview} Review)
-                  </div>
-                </div>
-              </li>
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">amazona</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Sopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">All right reserved.</footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductScreen} />
+            <Route path="/" exact component={Home} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/src/screens/ProductScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Data from '../Data';
+
+function ProductScreen(props) {
+  console.log(props.match.params.id);
+  const product = Data.products.find((x) => x._id === props.match.params.id);
+  return (
+    <div>
+      <div className="back-to-home">
+        <Link to="/">Back To Home</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt="Product" />
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4> {product.name} </h4>
+            </li>
+            <li>
+              {product.rating} Stars ({product.numReview} Review)
+            </li>
+            <li>
+              <b>Price: ${product.price}</b>
+            </li>
+            <li>
+              Description:
+              <div>{product.description}</div>
+            </li>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>Price: {product.price}</li>
+            <li>Status: {product.status}</li>
+            <li>
+              Qty:
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>{' '}
+            </li>
+            <li>
+              <button className="button primary">Add to cart</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductScreen;
